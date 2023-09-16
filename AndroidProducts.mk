d0249926ed1dc08ea25c9b4917823774c73327d8
GXC2356 <108477326+GXC2356@users.noreply.github.com>
2023-09-16 18:19:37 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/p404_vayu.mk
+    $(LOCAL_DIR)/vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    p404_vayu-eng \
-    p404_vayu-userdebug \
-    p404_vayu-user
+    vayu-eng \
+    vayu-userdebug \
+    vayu-user
